a046d83a2a7d1f3b88596aae463d5d193f71e136
Yusuf Kucukgokgozoglu <yusufcankggoglu@gmail.com>
2022-12-17 17:28:24 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_number1 = new System.Windows.Forms.Button();
+            this.btn_number2 = new System.Windows.Forms.Button();
+            this.btn_number3 = new System.Windows.Forms.Button();
+            this.btn_number6 = new System.Windows.Forms.Button();
+            this.btn_number5 = new System.Windows.Forms.Button();
+            this.btn_number4 = new System.Windows.Forms.Button();
+            this.btn_number9 = new System.Windows.Forms.Button();
+            this.btn_number8 = new System.Windows.Forms.Button();
+            this.btn_number7 = new System.Windows.Forms.Button();
+            this.btn_multiplication = new System.Windows.Forms.Button();
+            this.btn_subraction = new System.Windows.Forms.Button();
+            this.btn_sum = new System.Windows.Forms.Button();
+            this.btn_division = new System.Windows.Forms.Button();
+            this.btn_number0 = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.lbl_Screen = new System.Windows.Forms.Label();
+            this.btn_comma = new System.Windows.Forms.Button();
+            this.btn_clearElement = new System.Windows.Forms.Button();
+            this.btn_equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_number1
+            // 
+            this.btn_number1.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number1, "btn_number1");
+            this.btn_number1.ForeColor = System.Drawing.Color.Black;
+            this.btn_number1.Name = "btn_number1";
+            this.btn_number1.UseVisualStyleBackColor = false;
+            this.btn_number1.Click += new System.EventHandler(this.btn_number1_Click);
+            // 
+            // btn_number2
+            // 
+            this.btn_number2.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number2, "btn_number2");
+            this.btn_number2.ForeColor = System.Drawing.Color.Black;
+            this.btn_number2.Name = "btn_number2";
+            this.btn_number2.UseVisualStyleBackColor = false;
+            this.btn_number2.Click += new System.EventHandler(this.btn_number2_Click);
+            // 
+            // btn_number3
+            // 
+            this.btn_number3.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number3, "btn_number3");
+            this.btn_number3.ForeColor = System.Drawing.Color.Black;
+            this.btn_number3.Name = "btn_number3";
+            this.btn_number3.UseVisualStyleBackColor = false;
+            this.btn_number3.Click += new System.EventHandler(this.btn_number3_Click);
+            // 
+            // btn_number6
+            // 
+            this.btn_number6.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number6, "btn_number6");
+            this.btn_number6.ForeColor = System.Drawing.Color.Black;
+            this.btn_number6.Name = "btn_number6";
+            this.btn_number6.UseVisualStyleBackColor = false;
+            this.btn_number6.Click += new System.EventHandler(this.btn_number6_Click);
+            // 
+            // btn_number5
+            // 
+            this.btn_number5.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number5, "btn_number5");
+            this.btn_number5.ForeColor = System.Drawing.Color.Black;
+            this.btn_number5.Name = "btn_number5";
+            this.btn_number5.UseVisualStyleBackColor = false;
+            this.btn_number5.Click += new System.EventHandler(this.btn_number5_Click);
+            // 
+            // btn_number4
+            // 
+            this.btn_number4.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number4, "btn_number4");
+            this.btn_number4.ForeColor = System.Drawing.Color.Black;
+            this.btn_number4.Name = "btn_number4";
+            this.btn_number4.UseVisualStyleBackColor = false;
+            this.btn_number4.Click += new System.EventHandler(this.btn_number4_Click);
+            // 
+            // btn_number9
+            // 
+            this.btn_number9.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number9, "btn_number9");
+            this.btn_number9.ForeColor = System.Drawing.Color.Black;
+            this.btn_number9.Name = "btn_number9";
+            this.btn_number9.UseVisualStyleBackColor = false;
+            this.btn_number9.Click += new System.EventHandler(this.btn_number9_Click);
+            // 
+            // btn_number8
+            // 
+            this.btn_number8.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number8, "btn_number8");
+            this.btn_number8.ForeColor = System.Drawing.Color.Black;
+            this.btn_number8.Name = "btn_number8";
+            this.btn_number8.UseVisualStyleBackColor = false;
+            this.btn_number8.Click += new System.EventHandler(this.btn_number8_Click);
+            // 
+            // btn_number7
+            // 
+            this.btn_number7.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number7, "btn_number7");
+            this.btn_number7.ForeColor = System.Drawing.Color.Black;
+            this.btn_number7.Name = "btn_number7";
+            this.btn_number7.UseVisualStyleBackColor = false;
+            this.btn_number7.Click += new System.EventHandler(this.btn_number7_Click);
+            // 
+            // btn_multiplication
+            // 
+            this.btn_multiplication.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.btn_multiplication, "btn_multiplication");
+            this.btn_multiplication.ForeColor = System.Drawing.Color.Black;
+            this.btn_multiplication.Name = "btn_multiplication";
+            this.btn_multiplication.UseVisualStyleBackColor = false;
+            this.btn_multiplication.Click += new System.EventHandler(this.btn_multiplication_Click);
+            // 
+            // btn_subraction
+            // 
+            this.btn_subraction.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.btn_subraction, "btn_subraction");
+            this.btn_subraction.ForeColor = System.Drawing.Color.Black;
+            this.btn_subraction.Name = "btn_subraction";
+            this.btn_subraction.UseVisualStyleBackColor = false;
+            this.btn_subraction.Click += new System.EventHandler(this.btn_subraction_Click);
+            // 
+            // btn_sum
+            // 
+            this.btn_sum.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.btn_sum, "btn_sum");
+            this.btn_sum.ForeColor = System.Drawing.Color.Black;
+            this.btn_sum.Name = "btn_sum";
+            this.btn_sum.UseVisualStyleBackColor = false;
+            this.btn_sum.Click += new System.EventHandler(this.btn_sum_Click);
+            // 
+            // btn_division
+            // 
+            this.btn_division.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.btn_division, "btn_division");
+            this.btn_division.ForeColor = System.Drawing.Color.Black;
+            this.btn_division.Name = "btn_division";
+            this.btn_division.UseVisualStyleBackColor = false;
+            this.btn_division.Click += new System.EventHandler(this.btn_division_Click);
+            // 
+            // btn_number0
+            // 
+            this.btn_number0.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_number0, "btn_number0");
+            this.btn_number0.ForeColor = System.Drawing.Color.Black;
+            this.btn_number0.Name = "btn_number0";
+            this.btn_number0.UseVisualStyleBackColor = false;
+            this.btn_number0.Click += new System.EventHandler(this.btn_number0_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_clear, "btn_clear");
+            this.btn_clear.ForeColor = System.Drawing.Color.Black;
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // lbl_Screen
+            // 
+            this.lbl_Screen.BackColor = System.Drawing.Color.White;
+            this.lbl_Screen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.lbl_Screen, "lbl_Screen");
+            this.lbl_Screen.Name = "lbl_Screen";
+            // 
+            // btn_comma
+            // 
+            this.btn_comma.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.btn_comma, "btn_comma");
+            this.btn_comma.ForeColor = System.Drawing.Color.Black;
+            this.btn_comma.Name = "btn_comma";
+            this.btn_comma.UseVisualStyleBackColor = false;
+            this.btn_comma.Click += new System.EventHandler(this.btn_comma_Click);
+            // 
+            // btn_clearElement
+            // 
+            this.btn_clearElement.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_clearElement, "btn_clearElement");
+            this.btn_clearElement.ForeColor = System.Drawing.Color.Black;
+            this.btn_clearElement.Name = "btn_clearElement";
+            this.btn_clearElement.UseVisualStyleBackColor = false;
+            this.btn_clearElement.Click += new System.EventHandler(this.btn_clearElement_Click);
+            // 
+            // btn_equal
+            // 
+            this.btn_equal.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.btn_equal, "btn_equal");
+            this.btn_equal.ForeColor = System.Drawing.Color.Black;
+            this.btn_equal.Name = "btn_equal";
+            this.btn_equal.UseVisualStyleBackColor = false;
+            this.btn_equal.Click += new System.EventHandler(this.btn_equal_Click_1);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btn_equal);
+            this.Controls.Add(this.btn_clearElement);
+            this.Controls.Add(this.btn_comma);
+            this.Controls.Add(this.lbl_Screen);
+            this.Controls.Add(this.btn_division);
+            this.Controls.Add(this.btn_number0);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_multiplication);
+            this.Controls.Add(this.btn_subraction);
+            this.Controls.Add(this.btn_sum);
+            this.Controls.Add(this.btn_number9);
+            this.Controls.Add(this.btn_number8);
+            this.Controls.Add(this.btn_number7);
+            this.Controls.Add(this.btn_number6);
+            this.Controls.Add(this.btn_number5);
+            this.Controls.Add(this.btn_number4);
+            this.Controls.Add(this.btn_number3);
+            this.Controls.Add(this.btn_number2);
+            this.Controls.Add(this.btn_number1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_number1;
+        private System.Windows.Forms.Button btn_number2;
+        private System.Windows.Forms.Button btn_number3;
+        private System.Windows.Forms.Button btn_number6;
+        private System.Windows.Forms.Button btn_number5;
+        private System.Windows.Forms.Button btn_number4;
+        private System.Windows.Forms.Button btn_number9;
+        private System.Windows.Forms.Button btn_number8;
+        private System.Windows.Forms.Button btn_number7;
+        private System.Windows.Forms.Button btn_multiplication;
+        private System.Windows.Forms.Button btn_subraction;
+        private System.Windows.Forms.Button btn_sum;
+        private System.Windows.Forms.Button btn_division;
+        private System.Windows.Forms.Button btn_number0;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Label lbl_Screen;
+        private System.Windows.Forms.Button btn_comma;
+        private System.Windows.Forms.Button btn_clearElement;
+        private System.Windows.Forms.Button btn_equal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private decimal firstvalue;
+        private char operation;
+        private decimal secondvalue;
+        private void btn_number1_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text=="0")
+            {
+               lbl_Screen.Text = ""; 
+            }
+            lbl_Screen.Text += "1";
+        }
+
+        private void btn_number2_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "2";
+        }
+
+        private void btn_number3_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "3";
+        }
+
+        private void btn_number4_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "4";
+        }
+
+        private void btn_number5_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "5";
+        }
+
+        private void btn_number6_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "6";
+        }
+
+        private void btn_number7_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "7";
+        }
+
+        private void btn_number8_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "8";
+        }
+
+        private void btn_number9_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "9";
+        }
+
+        private void btn_number0_Click(object sender, EventArgs e)
+        {
+            if (lbl_Screen.Text == "0")
+            {
+                lbl_Screen.Text = "";
+            }
+            lbl_Screen.Text += "0";
+        }
+
+        private void btn_sum_Click(object sender, EventArgs e)
+        {
+            firstvalue = Convert.ToDecimal(lbl_Screen.Text);
+            operation = '+';
+            lbl_Screen.Text = "";
+        }
+
+        private void btn_subraction_Click(object sender, EventArgs e)
+        {
+            firstvalue = Convert.ToDecimal(lbl_Screen.Text);
+            operation = '-';
+            lbl_Screen.Text = "";
+        }
+
+        private void btn_multiplication_Click(object sender, EventArgs e)
+        {
+            firstvalue = Convert.ToDecimal(lbl_Screen.Text);
+            operation = '*';
+            lbl_Screen.Text = "";
+        }
+
+        private void btn_division_Click(object sender, EventArgs e)
+        {
+            firstvalue = Convert.ToDecimal(lbl_Screen.Text);
+            operation = '/';
+            lbl_Screen.Text = "";
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            lbl_Screen.Text = "0";
+            firstvalue = 0;
+            secondvalue = 0;
+            operation = ' ';
+        }
+
+        private void btn_comma_Click(object sender, EventArgs e)
+        {
+            lbl_Screen.Text += ",";
+        }
+
+        private void btn_equal_Click_1(object sender, EventArgs e)
+        {
+            secondvalue = Convert.ToDecimal(lbl_Screen.Text);
+            decimal result;
+            switch (operation)
+            {
+                case '+':
+                    result = firstvalue + secondvalue;
+                    break;
+                case '-':
+                    result = firstvalue - secondvalue;
+                    break;
+                case '*':
+                    result = firstvalue * secondvalue;
+                    break;
+                case '/':
+                    result = firstvalue / secondvalue;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            lbl_Screen.Text = Convert.ToString(result);
+        }
+
+        private void btn_clearElement_Click(object sender, EventArgs e)
+        {
+            lbl_Screen.Text = "0";
+        }
+    }
+}
